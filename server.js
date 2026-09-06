a7b01e83bdce1fbeee757393027341b18677c97b
XYCoolcraft <satriahaidaralvaro75@gmail.com>
2026-09-06 22:29:44 +0700

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const http = require("node:http");
 const fs = require("node:fs");
 const path = require("node:path");
 
-const ROOT = __dirname;
+const ROOT = path.join(__dirname, "index.html"); // Ganti "public" dengan nama folder HTML Anda
 const PORT = Number(process.env.PORT) || 3000;
 const HOST = process.env.HOST || "0.0.0.0";
 
